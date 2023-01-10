cf70af8b7e1cb4a0fc7346a584ab1d2f5db42bc9
Dimitris Kal <dimitriskaliampos@gmail.com>
2023-01-10 12:40:00 +0200

    Make changes to both files to fix requesting one more move

--- myscripts.js
+++ myscripts.js
@@ -1,67 +1,70 @@
 function getComputerChoice() {
     let pcChoice = Math.floor((Math.random() * 3) + 1);
-    // console.log(pcChoice);
+    
     if (pcChoice == 1) {
+        console.log("pc choice is " + pcChoice);
         return "rock";
     } 
     else if (pcChoice === 2) {
+        console.log("pc choice is " + pcChoice);
         return "paper";
     }
-    else {
+    else if (pcChoice === 3) {
+        console.log("pc choice is " + pcChoice);
         return "scissor";
     }
 }
 
 function getPlayerChoice() {
-   let move = prompt("What move would you like to make?");
-   move = move.toLowerCase();
-   console.log(move);
-   return move;
-}
+    let move = prompt("What move would you like to make?");
+    move = move.toLowerCase();
+    console.log("player move is " + move);
+    return move;
+ }
 
-let playerSelection = getPlayerChoice();
-let computerSelection = getComputerChoice();
 let playerWins = 0;
 let computerWins = 0;
 
-function playRound( playerSelection, computerSelection) {
-    computerSelection = getComputerChoice();
-    playerSelection = getPlayerChoice();
-    // let playerWins = 0;
-    // let computerWins = 0;
-    if (computerSelection === playerSelection) {
+
+function playRound() {
+    let computerMove = getComputerChoice();
+    let playerMove = getPlayerChoice();
+    if (computerMove === playerMove) {
         return "It's a tie! \nPlayer wins: " + playerWins + "\nComputer Wins: " + computerWins;
     } 
-    else if ((computerSelection === "rock") && (playerSelection === "paper")) {
+    else if (computerMove === "rock" &&playerMove === "paper") {
         playerWins++;
         return "You Win! Paper beats Rock! \nPlayer wins: " + playerWins + "\nComputer Wins: " + computerWins;
-    } 
-    else if ((computerSelection === "paper") && (playerSelection === "rock")) {
-        computerWins++;
-        return "You Lose! Paper beats Rock! \nPlayer wins: " + playerWins + "\nComputer Wins: " + computerWins;
     }
-    else if ((computerSelection === "scissor") && (playerSelection === "paper")) {
-        computerWins++;
-        return "You Lose! Scissor beats Paper! \nPlayer wins: " + playerWins + "\nComputer Wins: " + computerWins;
-    }
-    else if ((computerSelection === "scissor") && (playerSelection === "rock")) {
-        playerWins++;
-        return "You Win! Rock beats Scissor!  \nPlayer wins: " + playerWins + "\nComputer Wins: " + computerWins;
-    }
-    else if ((computerSelection === "rock") && (playerSelection === "scissor")) {
+    else if (computerMove === "rock" && playerMove === "scissor") {
         computerWins++;
         return "You Lose! Rock beats Paper! \nPlayer wins: " + playerWins + "\nComputer Wins: " + computerWins;
     }
-    else if ((computerSelection === "paper") && (playerSelection === "scissor")) {
+    else if (computerMove === "paper" && playerMove === "rock") {
+        computerWins++;
+        return "You Lose! Paper beats Rock! \nPlayer wins: " + playerWins + "\nComputer Wins: " + computerWins;
+    }
+    else if (computerMove === "scissor" && playerMove === "paper") {
+        computerWins++;
+        return "You Lose! Scissor beats Paper! \nPlayer wins: " + playerWins + "\nComputer Wins: " + computerWins;
+    }
+    else if (computerMove === "scissor" && playerMove === "rock") {
+        playerWins++;
+        return "You Win! Rock beats Scissor!  \nPlayer wins: " + playerWins + "\nComputer Wins: " + computerWins;
+    }
+    else if (computerMove === "paper" && playerMove === "scissor") {
         playerWins++;
         return "You Win! Scissor beats Paper! \nPlayer wins: " + playerWins + "\nComputer Wins: " + computerWins;
+    }
+    else {
+        return "Not a valid move :) ";
     }
 }
 
 
 
 function game() {
-        console.log(playRound(playerSelection, computerSelection))
+        console.log(playRound());
 }
 
 for ( let i = 0; i < 5; i++ ) {
